323cd6111cd73774c23c4c7441363d9091dd8c19
Roman Birg <romanbirg@gmail.com>
2012-12-11 04:12:41 -0600

    SystemUI: add left and right virtual buttons while typing
    [cyanogen] refactor for cm-14.0
    [LuK1337] update for 8.1
    [sam3000] Updates for ten
    
    * Removed keyRepeat attribute from layouts because it defaults to true
      and AOSP removed it anyway in commit 160ad6b
    
    * Removed layout-sw600dp layout changes because AOSP refactored to use
      layout instead as seen in commit aa94d95b
    
    * Renamed rot90 layout suffix to vertical as per AOSP changes
    
    [maniac103] More refactorings for ten
    
    * Follow new system of inflating all views dynamically
    
    Squashes the following commits:
    
    Author: Danny Baumann <dannybaumann@web.de>
    Date:   Wed Aug 6 09:16:35 2014 +0200
    
        Fix side button visibility tracking.
    
        Previously one could e.g. get legacy menu button being visible in
        certain scenarios, like this one:
        - Open legacy app, e.g. CatLog
        - Invoke IME
        - Lock screen
        - Unlock screen
        - Result: Menu button visible in keyguard
    
        Change-Id: Ia796f73ec995ea1cc34d38f8aefdbccda7be7565
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Wed Jul 2 09:31:23 2014 -0700
    
        SystemUI: fix up navigation cursor keyevent flags
    
        Volume cursor controls use slightly different flags to keep the cursor
        within the field. Match them in the SystemUI for consistent (and better)
        behavior.
    
        Change-Id: I56d597f111ec611b0ca24a10f30575401fa76d18
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Thu Jul 3 12:04:40 2014 -0700
    
        SystemUI: fix up initial cursor down flags on nav bar
    
        We were using the regular flags for the initial ACTION_DOWN event so the
        FLAG_KEEP_TOUCH_MODE was not being passed and allowed the cursor to jump
        out of bounds slightly.
    
        Change-Id: I5b4d2a5e5e0ce562985fcc3b7b6fac8b4abc49da
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Thu Apr 19 01:14:21 2018 +0200
    
        SystemUI: Fix navigation bar arrows visibility handling
    
        * Fixes issue where input events would be sent
          while dpad_group view was set to be invisible.
    
        Change-Id: Ie802e9c8157505a2ad706332f0b2fe715caf428c
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Sun Apr 19 23:17:12 2020 +0100
    
        NavigationBarView: Fix cursor keys visibility with gestural mode enabled
    
        When gestural mode is enabled and the IME is being shown
        the cursor keys must be hidden, otherwise there's an overlap.
    
        Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/1730
        Change-Id: I2f34810d7b137573360a140bbf267028363e6a28
    
    Change-Id: Iea446443d33ff305765e47c86523b619676116ac

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarView.java
@@ -102,6 +102,8 @@ public class NavigationBarView extends FrameLayout implements
 
     private static final String NAVBAR_STYLE =
             "system:" + Settings.System.NAVBAR_STYLE;
+    private static final String NAVIGATION_BAR_MENU_ARROW_KEYS =
+            "system:" + Settings.System.NAVIGATION_BAR_MENU_ARROW_KEYS;
 
     final static boolean ALTERNATE_CAR_MODE_UI = false;
 
@@ -126,6 +128,8 @@ public class NavigationBarView extends FrameLayout implements
     private KeyButtonDrawable mHomeDefaultIcon;
     private KeyButtonDrawable mRecentIcon;
     private KeyButtonDrawable mDockedIcon;
+    private KeyButtonDrawable mCursorRightIcon;
+    private KeyButtonDrawable mCursorLeftIcon;
     private Context mLightContext;
     private int mLightIconColor;
     private int mDarkIconColor;
@@ -183,6 +187,9 @@ public class NavigationBarView extends FrameLayout implements
     private boolean mOverviewProxyEnabled;
     private boolean mShowSwipeUpUi;
     private UpdateActiveTouchRegionsCallback mUpdateActiveTouchRegionsCallback;
+
+    private boolean mShowCursorKeys;
+    private boolean mImeVisible;
 
     private class NavTransitionListener implements TransitionListener {
         private boolean mBackTransitioning;
@@ -295,6 +302,10 @@ public class NavigationBarView extends FrameLayout implements
         final ContextualButton accessibilityButton =
                 new ContextualButton(R.id.accessibility_button, mLightContext,
                         R.drawable.ic_sysbar_accessibility_button);
+        final ContextualButton cursorLeftButton = new ContextualButton(R.id.dpad_left,
+                mLightContext, R.drawable.ic_chevron_start);
+        final ContextualButton cursorRightButton = new ContextualButton(R.id.dpad_right,
+                mLightContext, R.drawable.ic_chevron_end);
         mContextualButtonGroup.addButton(imeSwitcherButton);
         mContextualButtonGroup.addButton(accessibilityButton);
         mRotationContextButton = new RotationContextButton(R.id.rotate_suggestion,
@@ -330,6 +341,8 @@ public class NavigationBarView extends FrameLayout implements
         mButtonDispatchers.put(R.id.ime_switcher, imeSwitcherButton);
         mButtonDispatchers.put(R.id.accessibility_button, accessibilityButton);
         mButtonDispatchers.put(R.id.menu_container, mContextualButtonGroup);
+        mButtonDispatchers.put(R.id.dpad_left, cursorLeftButton);
+        mButtonDispatchers.put(R.id.dpad_right, cursorRightButton);
         mDeadZone = new DeadZone(this);
     }
 
@@ -444,6 +457,14 @@ public class NavigationBarView extends FrameLayout implements
         return mButtonDispatchers.get(R.id.home_handle);
     }
 
+    public ButtonDispatcher getCursorLeftButton() {
+        return mButtonDispatchers.get(R.id.dpad_left);
+    }
+
+    public ButtonDispatcher getCursorRightButton() {
+        return mButtonDispatchers.get(R.id.dpad_right);
+    }
+
     public SparseArray<ButtonDispatcher> getButtonDispatchers() {
         return mButtonDispatchers;
     }
@@ -475,6 +496,8 @@ public class NavigationBarView extends FrameLayout implements
         }
         if (densityChange || dirChange) {
             mRecentIcon = getDrawable(R.drawable.ic_sysbar_recent);
+            mCursorLeftIcon = getDrawable(R.drawable.ic_chevron_start);
+            mCursorRightIcon = getDrawable(R.drawable.ic_chevron_end);
             mContextualButtonGroup.updateIcons(mLightIconColor, mDarkIconColor);
         }
         if (orientationChange || densityChange || dirChange) {
@@ -591,6 +614,7 @@ public class NavigationBarView extends FrameLayout implements
         if (!visible) {
             mTransitionListener.onBackAltCleared();
         }
+        mImeVisible = visible;
         mRotationButtonController.getRotationButton().setCanShowRotationButton(!visible);
     }
 
@@ -619,18 +643,25 @@ public class NavigationBarView extends FrameLayout implements
         KeyButtonDrawable backIcon = mBackIcon;
         orientBackButton(backIcon);
         KeyButtonDrawable homeIcon = mHomeDefaultIcon;
+        KeyButtonDrawable cursorLeftIcon = mCursorLeftIcon;
+        KeyButtonDrawable cursorRightIcon = mCursorRightIcon;
         if (!mUseCarModeUi) {
             orientHomeButton(homeIcon);
         }
         getHomeButton().setImageDrawable(homeIcon);
         getBackButton().setImageDrawable(backIcon);
+        getCursorLeftButton().setImageDrawable(cursorLeftIcon);
+        getCursorRightButton().setImageDrawable(cursorRightIcon);
 
         updateRecentsIcon();
+
+        boolean disableCursorKeys = !mShowCursorKeys || !useAltBack ||
+                (QuickStepContract.isGesturalMode(mNavBarMode) && mImeVisible);
 
         // Update IME button visibility, a11y and rotate button always overrides the appearance
         boolean disableImeSwitcher =
                 (mNavigationIconHints & StatusBarManager.NAVIGATION_HINT_IME_SWITCHER_SHOWN) == 0
-                || isImeRenderingNavButtons();
+                || isImeRenderingNavButtons() || !disableCursorKeys;
         mContextualButtonGroup.setButtonVisibility(R.id.ime_switcher, !disableImeSwitcher);
 
         mBarTransitions.reapplyDarkIntensity();
@@ -676,6 +707,8 @@ public class NavigationBarView extends FrameLayout implements
         getHomeButton().setVisibility(disableHome       ? View.INVISIBLE : View.VISIBLE);
         getRecentsButton().setVisibility(disableRecent  ? View.INVISIBLE : View.VISIBLE);
         getHomeHandle().setVisibility(disableHomeHandle ? View.INVISIBLE : View.VISIBLE);
+        getCursorLeftButton().setVisibility(disableCursorKeys  ? View.INVISIBLE : View.VISIBLE);
+        getCursorRightButton().setVisibility(disableCursorKeys ? View.INVISIBLE : View.VISIBLE);
         notifyActiveTouchRegions();
     }
 
@@ -1106,6 +1139,7 @@ public class NavigationBarView extends FrameLayout implements
         reorient();
         final TunerService tunerService = Dependency.get(TunerService.class);
         tunerService.addTunable(this, NAVBAR_STYLE);
+        tunerService.addTunable(this, NAVIGATION_BAR_MENU_ARROW_KEYS);
         if (mRotationButtonController != null) {
             mRotationButtonController.registerListeners(false /* registerRotationWatcher */);
         }
@@ -1129,6 +1163,9 @@ public class NavigationBarView extends FrameLayout implements
     public void onTuningChanged(String key, String newValue) {
         if (NAVBAR_STYLE.equals(key)) {
             reloadNavIcons();
+        } else if (NAVIGATION_BAR_MENU_ARROW_KEYS.equals(key)) {
+            mShowCursorKeys = TunerService.parseIntegerSwitch(newValue, false);
+            setNavigationIconHints(mNavigationIconHints);
         }
     }
 
@@ -1169,6 +1206,8 @@ public class NavigationBarView extends FrameLayout implements
         dumpButton(pw, "rota", getRotateSuggestionButton());
         dumpButton(pw, "a11y", getAccessibilityButton());
         dumpButton(pw, "ime", getImeSwitchButton());
+        dumpButton(pw, "curl", getCursorLeftButton());
+        dumpButton(pw, "curr", getCursorRightButton());
 
         if (mNavigationInflaterView != null) {
             mNavigationInflaterView.dump(pw);
