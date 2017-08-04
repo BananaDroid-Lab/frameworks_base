7b091253b1da0700f1b694500c8df14aaaca93cc
LorDClockaN <lordclockan@gmail.com>
2017-08-04 15:43:57 +0200

    FWB: Sensor block per-package switch (1/2)
    Change-Id: I3a5cb8820ca6815c7140c25368e525faa2ecc5e6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6761,6 +6761,12 @@ public final class Settings {
         public static final String ENABLE_FLOATING_ROTATION_BUTTON = "enable_floating_rotation_button";
 
         /**
+         * Sensor block per-package
+         * @hide
+         */
+        public static final String SENSOR_BLOCK = "sensor_block";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -6928,6 +6934,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(OMNI_KEYGUARD_SHOW_BATTERY_BAR);
             PRIVATE_SETTINGS.add(OMNI_KEYGUARD_SHOW_BATTERY_BAR_ALWAYS);
             PRIVATE_SETTINGS.add(BACK_GESTURE_HAPTIC);
+            PRIVATE_SETTINGS.add(SENSOR_BLOCK);
         }
 
         /**
