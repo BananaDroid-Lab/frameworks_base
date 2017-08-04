6cdeb1f009d44fe3699f47d438a7169a97821d35
LorDClockaN <lordclockan@gmail.com>
2017-08-04 15:43:57 +0200

    FWB: Sensor block per-package switch (1/2)
    Change-Id: I3a5cb8820ca6815c7140c25368e525faa2ecc5e6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7054,6 +7054,12 @@ public final class Settings {
                 "lockscreen_quick_unlock_control";
 
         /**
+         * Sensor block per-package
+         * @hide
+         */
+        public static final String SENSOR_BLOCK = "sensor_block";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -7195,6 +7201,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(KEY_EDGE_LONG_SWIPE_ACTION);
             PRIVATE_SETTINGS.add(BACK_GESTURE_HAPTIC);
 	    PRIVATE_SETTINGS.add(OMNIJAWS_WEATHER_ICON_PACK);
+            PRIVATE_SETTINGS.add(SENSOR_BLOCK);
         }
 
         /**
