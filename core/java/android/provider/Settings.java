0a7866192a6ac6f88eb622c33f3c6d05e1d2a3b5
htchoi1995 <htchoi1995@gmail.com>
2020-10-06 19:49:18 +0000

    Disable/Enable screenshot sound [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6027,6 +6027,13 @@ public final class Settings {
         public static final String NAVBAR_STYLE = "navbar_style";
 
         /**
+         * Enable/Disable screenshot sound
+         * @hide
+         */
+        @Readable
+        public static final String SCREENSHOT_SOUND = "screenshot_sound";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
