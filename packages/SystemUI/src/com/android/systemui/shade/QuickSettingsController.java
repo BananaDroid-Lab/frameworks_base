fb94a2f089ed7b6c5c07da219d60f5af91cd456b
00day0 <therandomuser11@gmail.com>
2020-10-09 23:34:40 +0200

    SystemUI: OOS style notification clear all button [1/2]
    @neobuddy89:
    * Adapt with 13 monet colors.
    * Polished icons from material design icons.
    * Remove unneccessary cruft.
    * Fix stray OOS button on headsup.
    * Add NPE checks
    
    Change-Id: I399615fa574c46412ba23ce62d8562cf97ee581d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/QuickSettingsController.java
+++ packages/SystemUI/src/com/android/systemui/shade/QuickSettingsController.java
@@ -1067,6 +1067,7 @@ public class QuickSettingsController implements Dumpable {
         mShadeHeaderController.setQsExpandedFraction(qsExpansionFraction);
         mShadeHeaderController.setQsVisible(mVisible);
         mPanelViewControllerLazy.get().reTickerViewVisibility();
+        mPanelViewControllerLazy.get().updateDismissAllVisibility();
 
         // Update the light bar
         mLightBarController.setQsExpanded(mFullyExpanded);
