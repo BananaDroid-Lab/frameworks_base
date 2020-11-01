e8572253034503c3e9c24a53fbe3e5dbc1ec48ce
Ido Ben-Hur <idoybh2@gmail.com>
2020-11-01 17:28:34 +0200

    base: Allow customizing volume dialog timeout [1/2]
    Also reduce show & hide animation by a bit
    
    Change-Id: Idb9e9147d999996dac5d78996f4492d7aa1c515d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6989,6 +6989,13 @@ public final class Settings {
         public static final String SMART_PIXELS_ON_POWER_SAVE = "smart_pixels_on_power_save";
 
         /**
+         * Volume dialog timeout. Value in seconds
+         * @hide
+         */
+        @Readable
+        public static final String VOLUME_DIALOG_TIMEOUT = "volume_dialog_timeout";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
