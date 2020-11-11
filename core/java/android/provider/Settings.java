1283d6bde244335f96060bdd04bd0e5aebb5c916
maxwen <max.weninger@gmail.com>
2020-11-11 16:04:30 +0100

    Allow changing back gesture height [1/2]
    * neobuddy89: Simplify and cleanup!
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6928,6 +6928,9 @@ public final class Settings {
          */
         public static final String ISLAND_NOTIFICATION = "island_notification";
 
+        /** @hide */
+        public static final String BACK_GESTURE_HEIGHT = "back_gesture_height";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
