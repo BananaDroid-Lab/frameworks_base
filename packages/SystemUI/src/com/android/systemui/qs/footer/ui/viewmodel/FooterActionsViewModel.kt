046b6e947359b4235a799b098b83bbdf8ab5340d
Ido Ben-Hur <idoybh2@gmail.com>
2021-11-26 18:42:20 +0200

    QSFooter: Launch Banana Gear when long clicking settings
    Needed a rewrite after r35
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    fixup! QSFooter: Launch Banana Gear when long clicking settings
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/footer/ui/viewmodel/FooterActionsViewModel.kt
+++ packages/SystemUI/src/com/android/systemui/qs/footer/ui/viewmodel/FooterActionsViewModel.kt
@@ -157,6 +157,7 @@ class FooterActionsViewModel(
                 ),
             backgroundColor = R.attr.offStateColor,
             this::onSettingsButtonClicked,
+            this::onSettingsButtonLongClicked
         )
 
     /** The model for the power button. */
@@ -175,6 +176,7 @@ class FooterActionsViewModel(
                     ),
                 backgroundColor = com.android.internal.R.attr.colorAccent,
                 this::onPowerButtonClicked,
+                this::doNothingLongClick,
             )
         } else {
             null
@@ -250,6 +252,18 @@ class FooterActionsViewModel(
         footerActionsInteractor.showSettings(expandable)
     }
 
+    private fun onSettingsButtonLongClicked(expandable: Expandable): Boolean {
+        if (falsingManager.isFalseTap(FalsingManager.LOW_PENALTY)) {
+            return false
+        }
+
+        return footerActionsInteractor.showCustomSettings(expandable)
+    }
+
+    private fun doNothingLongClick(expandable: Expandable): Boolean {
+        return false
+    }
+
     private fun onPowerButtonClicked(expandable: Expandable) {
         val mKeyguard = Dependency.get(KeyguardStateController::class.java)
         if (mKeyguard.isShowing() && mKeyguard.isMethodSecure() 
@@ -281,6 +295,7 @@ class FooterActionsViewModel(
             iconTint = null,
             backgroundColor = R.attr.offStateColor,
             onClick = this::onUserSwitcherClicked,
+            onLongClick = this::doNothingLongClick,
         )
     }
 
