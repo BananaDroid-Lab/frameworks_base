b9a15ccac71e86db5ce8aa60b96f07b746b3001c
Rishawn <70256146+Stealth1226@users.noreply.github.com>
2022-01-10 09:47:15 +0530

    SystemUI: Allow disabling ripple effect on unlock [1/2]
    Squashed:
    
    Author: Nauval Rizky <enuma.alrizky@gmail.com>
    Date:   Wed Jan 19 08:31:56 2022 +0700
    
        SystemUI: Fix method for disabling unlock ripple animation
    
        NotificationShadeWindowController's are not gracefully closed, which
        leads to top parts of the screen is being obscured by invisible shade window.
        And also, "ripple" is actually still exists due to lightRevealScrim are
        being started by onKeyguardFadingAwayChanged, which is not what user expect
        when they want to disable the effect.
    
        So in order to handle those cases, lets intercept it from the
        AuthRippleController instead.
    
        [palaych] With system settings we need to pass UserHandle.USER_CURRENT to make sure we are getting proper value.
    
        Change-Id: I85ab96368c8b0099700f3de18ba95ce63eaac443
    
    Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Date:   Fri Oct 27 07:08:57 2023 +0530
    
        SystemUI: Disable wakeup animations when unlocking with fingerprint
    
        If the ripple animation is disabled it makes sense to disable these too.
    
        Change-Id: I44542796acdb1dfa2fd7626b0a69386da499da2c
    
    Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Date:   Sat Nov 11 08:58:07 2023 +0530
    
        SystemUI: Disable keyguard unlock to launcher animation if...
    
        ...if the user disabled "Unlock animation" aka auth ripple.
    
        Change-Id: I824d9434bec0a577686e9365d54154f947167398
    
    Co-authored-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Change-Id: Idecaa59547159d0a2b0ecdb55a207377bd466710
    [ghostrider-reborn: use more appropriate setting name]
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6815,6 +6815,12 @@ public final class Settings {
         public static final String FINGERPRINT_SUCCESS_VIB = "fingerprint_success_vib";
 
         /**
+         * Whether to show the ripple animation on fingerprint unlock
+         * @hide
+         */
+        public static final String AUTH_RIPPLE_ENABLED = "auth_ripple_enabled";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
