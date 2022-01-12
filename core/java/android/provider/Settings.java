006c9983c916a1765c4e30bffa2980a3d3968101
SagarMakhar <sagarmakhar@gmail.com>
2022-01-12 18:32:54 +0000

    base: Black theme [1/3]
    Change-Id: Ib165e7ba0288eaad7a1e3c1109638acdb66fa05c
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13222,6 +13222,12 @@ public final class Settings {
         public static final String SHOW_CLIPBOARD_OVERLAY = "show_clipboard_overlay";
 
         /**
+         * Whether to turn on black theme
+         * @hide
+         */
+        public static final String SYSTEM_BLACK_THEME = "system_black_theme";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
