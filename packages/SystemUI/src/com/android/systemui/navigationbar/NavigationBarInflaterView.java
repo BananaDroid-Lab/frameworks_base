e7dbd5559637fea43556d81a4491e216a1c7b7f2
Pranav Vashi <neobuddy89@gmail.com>
2022-03-08 20:55:15 +0530

    NavigationBarInflaterView: Improve applying overlays
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarInflaterView.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarInflaterView.java
@@ -188,8 +188,10 @@ public class NavigationBarInflaterView extends FrameLayout
     }
 
     private void onNavigationModeChanged(int mode) {
-        mNavBarMode = mode;
-        onLikelyDefaultLayoutChange();
+        if (mNavBarMode != mode) {
+            mNavBarMode = mode;
+            onLikelyDefaultLayoutChange(false);
+        }
     }
 
     @Override
@@ -224,9 +226,7 @@ public class NavigationBarInflaterView extends FrameLayout
             updateLayoutInversion();
         } else if (NAVBAR_LAYOUT_VIEWS.equals(key)) {
             mNavBarLayout = (String) newValue;
-            if (!QuickStepContract.isGesturalMode(mNavBarMode)) {
-                setNavigationBarLayout(mNavBarLayout);
-            }
+            onLikelyDefaultLayoutChange(true);
         }
     }
 
@@ -236,17 +236,17 @@ public class NavigationBarInflaterView extends FrameLayout
         updateLayoutInversion();
     }
 
-    private void setNavigationBarLayout(String layoutValue) {
-        if (!Objects.equals(mCurrentLayout, layoutValue)) {
+    private void setNavigationBarLayout(String layoutValue, boolean force) {
+        if (!Objects.equals(mCurrentLayout, layoutValue) || force) {
             clearViews();
             inflateLayout(layoutValue);
         }
     }
 
-    public void onLikelyDefaultLayoutChange() {
-        setNavigationBarLayout(getDefaultLayout());
+    public void onLikelyDefaultLayoutChange(boolean force) {
+        setNavigationBarLayout(getDefaultLayout(), force);
         if (!QuickStepContract.isGesturalMode(mNavBarMode)) {
-            setNavigationBarLayout(mNavBarLayout);
+            setNavigationBarLayout(mNavBarLayout, force);
         }
     }
 
