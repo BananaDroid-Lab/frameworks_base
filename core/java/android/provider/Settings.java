d3c1135c6e98fb4dfda4a765bdcbf4a34920fcbf
Pranav Vashi <neobuddy89@gmail.com>
2022-03-09 00:19:33 +0530

    Reload navbar icons on changing style [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6821,6 +6821,12 @@ public final class Settings {
         public static final String AUTH_RIPPLE_ENABLED = "auth_ripple_enabled";
 
         /**
+         * Navbar style
+         * @hide
+         */
+        public static final String NAVBAR_STYLE = "navbar_style";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
