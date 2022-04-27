70a47206410cea8020f961b8d83bd44fdd834c5d
maxwen <max.weninger@gmail.com>
2022-04-27 17:33:18 +0800

    SystemUI: QS customizations settings [1/2]
    includes:
    
    [maxwenn & cjh1249131356]
    * SystemUI: hide label and vertical layout settings
    * base: QS tiles layout customizations
    
    - Inspired from https://github.com/omnirom/android_frameworks_base/commit/4d71992f4446d828e0f5e7eb3841632ea28ee71c.
      To avoid breaking aosp performance of calculating stuff here and there, use overlay to implement this.
    
    [minaripenguin]
    * SystemUI: Fix qs tile vertical layout scrolling
    * SystemUI: Simpflify updateLayout and debrand QSLayoutUtils/overlays to com.android
    * Re-evaluate hide label and vertical layout settings on settings change
    * SystemUI: Use DelayableMarqueeTextView util for vertical qs tile as well
    
    [timjosten]
    * SystemUI: Improve vertical QS tile layout & animation
    
    Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
    Co-authored-by: timjosten <timjosten@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Change-Id: Ib6dcddd8edcacc93caaef3cf550b50196e46097c
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -108,5 +108,9 @@ public class SystemSettings {
         Settings.System.NOTIFICATION_COOLDOWN_ALL,
         Settings.System.NOTIFICATION_COOLDOWN_VIBRATE_UNLOCKED,
         Settings.System.QS_PANEL_STYLE,
+        Settings.System.QS_TILE_LABEL_HIDE,
+        Settings.System.QS_TILE_VERTICAL_LAYOUT,
+        Settings.System.QS_LAYOUT,
+        Settings.System.QQS_LAYOUT,
     };
 }
