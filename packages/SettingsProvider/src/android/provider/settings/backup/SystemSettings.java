ca23188b2767ac758c578172cf69a137ca9f2b24
maxwen <max.weninger@gmail.com>
2022-04-27 17:33:18 +0800

    SystemUI: QS vertical layout & hide label settings [1/2]
    travarilo: Use TunerService
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -89,5 +89,7 @@ public class SystemSettings {
         Settings.System.NOTIFICATION_LIGHT_PULSE,
         Settings.System.MEDIA_ARTWORK_BLUR_ENABLED,
         Settings.System.MEDIA_ARTWORK_BLUR_RADIUS,
+        Settings.System.QS_TILE_LABEL_HIDE,
+        Settings.System.QS_TILE_VERTICAL_LAYOUT,
     };
 }
