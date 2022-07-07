c576df5b0eff453c1c3c93335e3490798ebf1a14
maxwen <max.weninger@gmail.com>
2022-07-07 16:48:06 +0400

    base: allow disable of screenshot shutter sound [1/2]
    @neobuddy89: Updated for A13.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5847,6 +5847,12 @@ public final class Settings {
 	public static final String LAUNCHER_SHOW_WEATHER_TEMP = "launcher_show_weather_temp";
 
         /**
+         * Enable/Disable screenshot sound
+         * @hide
+         */
+        public static final String SCREENSHOT_SHUTTER_SOUND = "screenshot_shutter_sound";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
