5fa32c8eeb74eb0403bc0a94c05e3d43c353ab16
vjspranav <pranavasri@live.in>
2022-07-13 01:57:15 +0200

    base: Add toggle for custom Settings UI [1/3]
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Change-Id: I5dea89158b0ba45be958c1b3dce94e932f98b87e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6504,6 +6504,15 @@ public final class Settings {
         public static final String STATUSBAR_BATTERY_BAR_BLEND_COLOR_REVERSE =
                 "statusbar_battery_bar_blend_color_reverse";
 
+	/**
+          * Enable disable CustomUI
+          *   0 = disabled
+          *   1 = enabled
+          * @author vjspranav
+          * @hide
+          */
+         public static final String CUSTOM_UI_TOGGLE = "custom_ui_toggle";
+
         /**
          * reTicker Status
          * @hide
@@ -6973,6 +6982,7 @@ public final class Settings {
 	    PRIVATE_SETTINGS.add(OMNIJAWS_WEATHER_ICON_PACK);
             PRIVATE_SETTINGS.add(BUTTON_BACKLIGHT_ONLY_WHEN_PRESSED);
             PRIVATE_SETTINGS.add(LOCK_HIDE_STATUS_BAR);
+            PRIVATE_SETTINGS.add(CUSTOM_UI_TOGGLE);
         }
 
         /**
