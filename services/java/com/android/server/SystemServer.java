f11af1db1b3c5735940f7456acc52cc0e4581805
Erfan Abdi <erfangplus@gmail.com>
2022-07-21 18:02:27 +0430

    base: Introduce LiveDisplay from Lineage
    Change-Id: Ie3a61a67edfc4f4568fa2341791fa9fa5263f90d

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -224,6 +224,7 @@ import com.android.server.webkit.WebViewUpdateService;
 import com.android.server.wm.ActivityTaskManagerService;
 import com.android.server.wm.WindowManagerGlobalLock;
 import com.android.server.wm.WindowManagerService;
+import com.android.server.lineage.display.LiveDisplayService;
 
 import dalvik.system.VMRuntime;
 
@@ -2572,6 +2573,10 @@ public final class SystemServer implements Dumpable {
             // LineageHardware
             t.traceBegin("StartLineageHardwareService");
             mSystemServiceManager.startService(LineageHardwareService.class);
+            t.traceEnd();
+
+            t.traceBegin("StartLiveDisplayService");
+            mSystemServiceManager.startService(LiveDisplayService.class);
             t.traceEnd();
         }
 
