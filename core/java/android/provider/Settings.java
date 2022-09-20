1722cc22c3b0fdb6c43a4123887826703c822e2a
vjspranav <pranavasri@live.in>
2022-09-20 17:34:48 +0000

    Allow changing system themes [1/2]
    * Currently Black,Vivid,Clear(WIP)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13083,6 +13083,12 @@ public final class Settings {
         public static final String SHOW_CLIPBOARD_OVERLAY = "show_clipboard_overlay";
 
         /**
+         * Switch to change custom system theme
+         * @hide
+         */
+        public static final String SYSTEM_CUSTOM_THEME = "system_custom_theme";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
