e19e360cb41d96d5a6472e1b90ba493329175d3d
minaripenguin <minaripenguin@users.noreply.github.com>
2023-05-08 11:34:34 +0800

    Status bar logo: Fix status bar left logo visibility
    fixes: status bar left logo occupying space after keyguard unlock
    
    Change-Id: I39a3961a2691977d38bfd675a4ea371975be0214
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
@@ -85,6 +85,8 @@ import com.android.systemui.util.CarrierConfigTracker.CarrierConfigChangedListen
 import com.android.systemui.util.CarrierConfigTracker.DefaultDataSubscriptionChangedListener;
 import com.android.systemui.util.settings.SecureSettings;
 
+import com.android.systemui.banana.logo.LogoImage;
+
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -685,12 +687,16 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     }
 
     public void hideNotificationIconArea(boolean animate) {
-        animateHide(mLeftLogo, animate);
+    	if (LogoImage.getLogoPosition(getContext()) == 0) {
+            animateFullyHide(mLeftLogo, animate);
+        }
         animateHide(mNotificationIconAreaInner, animate);
     }
 
     public void showNotificationIconArea(boolean animate) {
-        animateShow(mLeftLogo, animate);
+    	if (LogoImage.getLogoPosition(getContext()) == 0) {
+            animateShow(mLeftLogo, animate);
+        }
         animateShow(mNotificationIconAreaInner, animate);
     }
 
@@ -732,6 +738,13 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         if (v == null)
             return;
         animateHiddenState(v, View.INVISIBLE, animate);
+    }
+
+    /**
+     * Hides a view and its hierarchy.
+     */
+    private void animateFullyHide(final View v, boolean animate) {
+        animateHiddenState(v, View.GONE, animate);
     }
 
     /**
