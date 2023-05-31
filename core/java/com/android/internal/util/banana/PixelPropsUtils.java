fcf000c4d01d285cee4796e976356bb933b4ddc9
Rizky Benggolo <travarilo@gmail.com>
2023-05-31 12:04:22 +0200

    Revert "PixelPropUtils: Spoof google assistant to cheetah"
    This reverts commit 3cde9eee036e6a2819e77faf6571aa7cc439a972.

--- core/java/com/android/internal/util/banana/PixelPropsUtils.java
+++ core/java/com/android/internal/util/banana/PixelPropsUtils.java
@@ -51,7 +51,6 @@ public class PixelPropsUtils {
 
     // Packages to Spoof as Pixel 7 Pro
     private static final String[] packagesToChangePixel7Pro = {
-            "com.google.android.apps.googleassistant",
             "com.google.android.apps.wallpaper",
             "com.google.android.apps.privacy.wildlife",
             "com.google.android.apps.subscriptions.red",
