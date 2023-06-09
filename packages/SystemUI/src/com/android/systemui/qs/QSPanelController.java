cbf02efcdb8af3138e964449946b7907e6fde563
Rizky Benggolo <travarilo@gmail.com>
2023-06-09 17:53:01 +0700

    Revert "SystemUI: Fix QS column count on landscape mode"
    This reverts commit 00fda7c2dd2be71e0ba6f38c60aaea9a688dd80d.

--- packages/SystemUI/src/com/android/systemui/qs/QSPanelController.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanelController.java
@@ -132,6 +132,9 @@ public class QSPanelController extends QSPanelControllerBase<QSPanel> {
             refreshAllTiles();
         }
         switchTileLayout(true);
+        if (mView != null) {
+            mView.updateColumns();
+        }
         mBrightnessMirrorHandler.onQsPanelAttached();
         PagedTileLayout pagedTileLayout= ((PagedTileLayout) mView.getOrCreateTileLayout());
         pagedTileLayout.setOnTouchListener(mTileLayoutTouchListener);
@@ -161,6 +164,7 @@ public class QSPanelController extends QSPanelControllerBase<QSPanel> {
     protected void onConfigurationChanged() {
     	if (mView != null) {
             mView.updateResources();
+            mView.updateColumns();
         }
         if (mView.isListening()) {
             refreshAllTiles();
