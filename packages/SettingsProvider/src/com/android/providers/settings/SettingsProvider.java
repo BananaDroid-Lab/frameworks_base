84705fde2c558d4f99d8122336c2299d1914b993
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2023-06-29 12:41:29 +0000

    SettingsProviderHooks: Infrastructure for hooking settings state init
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
+++ packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
@@ -3066,6 +3066,7 @@ public class SettingsProvider extends ContentProvider {
             // Upgrade the settings to the latest version.
             UpgradeController upgrader = new UpgradeController(userId);
             upgrader.upgradeIfNeededLocked();
+            SettingsProviderHooks.onSettingsStateInit(SettingsRegistry.this, userId);
             return true;
         }
 
