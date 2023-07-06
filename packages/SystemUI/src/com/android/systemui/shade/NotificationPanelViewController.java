ce42678b3ac2480d87dd4210e779fe20704e37f1
minaripenguin <minaripenguin@users.noreply.github.com>
2023-07-06 08:40:38 +0800

    fixup! SystemUI: Improve dismiss all button visiblity
    Change-Id: I15744364ffc80619b0f8a8d6b9f9214f3e793c6d
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -5428,7 +5428,6 @@ public final class NotificationPanelViewController implements Dumpable {
         if (visibility && mReTickerComeback.getVisibility() == View.VISIBLE) {
             // check if we can dismiss reticker
             retickerDismiss();
-            mReTickerVisible = false;
         }
 
         if (visibility && getExpandedFraction() != 1) {
@@ -5477,19 +5476,16 @@ public final class NotificationPanelViewController implements Dumpable {
                     }
                     retickerDismiss();
                     reTickerViewVisibility();
-                    mReTickerVisible = false;
                 });
             }
         } else {
             retickerDismiss();
-            mReTickerVisible = false;
         }
     }
 
     protected void reTickerViewVisibility() {
         if (!mReTickerStatus) {
             retickerDismiss();
-            mReTickerVisible = false;
             return;
         }
 
@@ -5506,6 +5502,7 @@ public final class NotificationPanelViewController implements Dumpable {
     }
 
     public void retickerAnimate() {
+        closeQsIfPossible();
         mCentralSurfaces.updateDismissAllVisibility(false);
 
         if (mIsAnimatingTicker) {
@@ -5562,7 +5559,6 @@ public final class NotificationPanelViewController implements Dumpable {
                 if (mIsDismissRequested) {
                     // Dismiss was requested during animation, trigger dismiss animation
                     retickerDismiss();
-                    mReTickerVisible = false;
                 }
             }
         });
