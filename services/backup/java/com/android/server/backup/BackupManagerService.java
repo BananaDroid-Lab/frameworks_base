LineageOS/android_frameworks_base

Enable backup service for non-owner users
Change-Id: I2595366345577623710851037281784051530469

services/backup/java/com/android/server/backup/BackupManagerService.java
@@ -42,6 +42,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.content.pm.UserInfo;
 import android.os.Binder;
 import android.os.FileUtils;
 import android.os.Handler;
@@ -54,6 +55,8 @@ import android.os.SystemProperties;
 import android.os.Trace;
 import android.os.UserHandle;
 import android.os.UserManager;
+import android.provider.Settings;
+import android.util.Log;
 import android.util.Slog;
 import android.util.SparseArray;
 
@@ -170,6 +173,19 @@ public class BackupManagerService extends IBackupManager.Stub {
 
     private boolean mHasFirstUserUnlockedSinceBoot = false;
 
+    private final BroadcastReceiver mUserAddedReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (Intent.ACTION_USER_ADDED.equals(intent.getAction())) {
+                int userId = intent.getIntExtra(Intent.EXTRA_USER_HANDLE, UserHandle.USER_NULL);
+                Log.d(TAG, "new user added User ID : " + userId);
+                if (userId > 0) {
+                mHandler.post(() -> setBackupServiceActive(userId, true));
+                }
+            }
+        }
+    };
+
     public BackupManagerService(Context context) {
         mContext = context;
         mGlobalDisable = isBackupDisabled();
@@ -189,6 +205,36 @@ public class BackupManagerService extends IBackupManager.Stub {
         if (DEBUG) {
             Slog.d(TAG, "Default backup user id = " + mDefaultBackupUserId);
         }
+
+        mContext.registerReceiver(
+                mUserAddedReceiver, new IntentFilter(Intent.ACTION_USER_ADDED));
+
+        if (shouldMigrateExistingUser(context)) {
+            List<UserInfo> managerUsers = mUserManager.getUsers();
+                for (UserInfo userInfo : managerUsers) {
+                    setBackupServiceActive(
+                        userInfo.getUserHandle().getIdentifier(),
+                        true
+                    );
+            }
+
+            markExistingUserMigrated(context);
+        }
+
+    }
+
+    private static final String PROFILE_MIGRATION_COMPLETED = "graphene_existing_users_migration";
+
+    private boolean markExistingUserMigrated(Context context) {
+        return Settings.Global.putInt(context.getContentResolver(), PROFILE_MIGRATION_COMPLETED, 1);
+    }
+
+    private boolean shouldMigrateExistingUser(Context context) {
+        return Settings.Global.getInt(
+                context.getContentResolver(),
+                PROFILE_MIGRATION_COMPLETED,
+                0
+            ) != 1;
     }
 
     // TODO: Remove this when we implement DI by injecting in the construtor.

BananaDroid-Lab/frameworks_base

services/backup/java/com/android/server/backup/BackupManagerService.java
@@ -42,6 +42,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.content.pm.UserInfo;
 import android.os.Binder;
 import android.os.FileUtils;
 import android.os.Handler;
@@ -54,6 +55,8 @@ import android.os.SystemProperties;
 import android.os.Trace;
 import android.os.UserHandle;
 import android.os.UserManager;
+import android.provider.Settings;
+import android.util.Log;
 import android.util.Slog;
 import android.util.SparseArray;
 
@@ -170,6 +173,19 @@ public class BackupManagerService extends IBackupManager.Stub {
 
     private boolean mHasFirstUserUnlockedSinceBoot = false;
 
+    private final BroadcastReceiver mUserAddedReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (Intent.ACTION_USER_ADDED.equals(intent.getAction())) {
+                int userId = intent.getIntExtra(Intent.EXTRA_USER_HANDLE, UserHandle.USER_NULL);
+                Log.d(TAG, "new user added User ID : " + userId);
+                if (userId > 0) {
+                mHandler.post(() -> setBackupServiceActive(userId, true));
+                }
+            }
+        }
+    };
+
     public BackupManagerService(Context context) {
         mContext = context;
         mGlobalDisable = isBackupDisabled();
@@ -189,6 +205,36 @@ public class BackupManagerService extends IBackupManager.Stub {
         if (DEBUG) {
             Slog.d(TAG, "Default backup user id = " + mDefaultBackupUserId);
         }
+
+        mContext.registerReceiver(
+                mUserAddedReceiver, new IntentFilter(Intent.ACTION_USER_ADDED));
+
+        if (shouldMigrateExistingUser(context)) {
+            List<UserInfo> managerUsers = mUserManager.getUsers();
+                for (UserInfo userInfo : managerUsers) {
+                    setBackupServiceActive(
+                        userInfo.getUserHandle().getIdentifier(),
+                        true
+                    );
+            }
+
+            markExistingUserMigrated(context);
+        }
+
+    }
+
+    private static final String PROFILE_MIGRATION_COMPLETED = "lineage_existing_users_migration";
+
+    private boolean markExistingUserMigrated(Context context) {
+        return Settings.Global.putInt(context.getContentResolver(), PROFILE_MIGRATION_COMPLETED, 1);
+    }
+
+    private boolean shouldMigrateExistingUser(Context context) {
+        return Settings.Global.getInt(
+                context.getContentResolver(),
+                PROFILE_MIGRATION_COMPLETED,
+                0
+            ) != 1;
     }
 
     // TODO: Remove this when we implement DI by injecting in the construtor.