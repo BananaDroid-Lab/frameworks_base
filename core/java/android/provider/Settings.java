d35094b4a75ffcf6cf53621c6dfee2cfc34a3120
Rizky Benggolo <travarilo@gmail.com>
2024-01-14 12:33:41 +0100

    base: Add toggle for about phone header style [1/3]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7288,6 +7288,12 @@ public final class Settings {
           */
         public static final String CUSTOM_UI_TOGGLE = "custom_ui_toggle";
 
+	/**
+          * About phone header style on Settings
+          * @hide
+          */
+        public static final String ABOUT_PHONE_HEADER = "about_phone_header";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
@@ -7454,6 +7460,7 @@ public final class Settings {
 	    PRIVATE_SETTINGS.add(OMNIJAWS_WEATHER_ICON_PACK);
             PRIVATE_SETTINGS.add(SENSOR_BLOCK);
             PRIVATE_SETTINGS.add(CUSTOM_UI_TOGGLE);
+            PRIVATE_SETTINGS.add(ABOUT_PHONE_HEADER);
         }
 
         /**
